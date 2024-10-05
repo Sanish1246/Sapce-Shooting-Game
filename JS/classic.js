4d640ef5b74f27ddaba72242ddda4c4954fadae2
Sanish1246 <Sanishferrari@gmail.com>
2024-10-05 10:01:11 +0400

    Enemy moving pattern

--- JS/classic.js
+++ JS/classic.js
@@ -15,15 +15,19 @@ let playerImg;
 let playerVelX=tile;
 
 let enemies=[];
-let enemyWidth=tile;
+let enemyWidth=tile*2;
 let enemyHeight=tile;
 let enemyX=tile;
 let enemyY=tile;
-let enemyImg
+let enemyImg;
+let enemyVelX=1;
 
 let enemyRow=2;
 let enemyCol=3;
 let remaining=0;
+
+let shotArray=[];
+let shootVelY=-10;
 
 let player= {
     y:playerY,
@@ -51,6 +55,7 @@ window.onload = ()=>{
 
     requestAnimationFrame(update);
     document.addEventListener("keydown",move);
+    document.addEventListener("keydup",shoot); //releases a bullet once the spacebar is released
 }
 
 function update(){ //Function to update the player position
@@ -61,8 +66,26 @@ function update(){ //Function to update the player position
     for(let i=0;i<enemies.length;i++){
         let enemy=enemies[i];
         if (enemy.alive) {
+            enemy.x+=enemyVelX;  //The enemy moves horizontally
+            if (enemy.x+enemy.width >=map.width||enemy.x<=0){
+                enemyVelX*=-1; //Inverting the movement direction on collision with the edge of the map
+                enemy.x += enemyVelX*2;
+
+                for (let i=0; i<enemies.length;i++) {
+                    enemies[i].y+=enemyHeight; //Making all the enemies descend by 1 tile
+                }
+            }
             context.drawImage(enemyImg, enemy.x,enemy.y,enemy.width,enemy.height);
         }
+    }
+
+    for(let j=0;j<shotArray.length;j++){
+        let shot=shotArray[j];
+        shot.y+=shootVelY;
+        let shotImg=new Image();
+        shotImg.src="../Images/shot.png";
+        context.drawImage(shotImg,shot.x,shot.y,shot.width,shot.heigth)
+
     }
 }
 
@@ -93,4 +116,17 @@ function createEnemy() { //creating the enemies and their positions
         }
     }
     remaining=enemies.length;
+}
+
+function shoot(e){
+    if (e.code=="Space"){
+        let bullet = {
+            x: player.x + playerWidth*15/32,
+            y:player.y,
+            width:tile/8,
+            heigth:tile/2,
+            hit:false
+        }
+        shotArray.push(bullet);
+    }
 }
